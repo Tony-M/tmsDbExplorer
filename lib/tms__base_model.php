7f454d308adc1306d86a4adda7dd61dba0de6a84
Морозов Антон <morozov@softkey.ru>
2015-09-01 16:42:27 +0300

    рабочие правки

--- lib/tms__base_model.php
+++ lib/tms__base_model.php
@@ -9,4 +9,12 @@ class tms__base_model {
     public function getFieldNames(){
         return array_keys($this->DATA);
     }
+
+    /**
+     * @param text $name
+     * @return bool
+     */
+    public function isFieldExist($name=null){
+        return isset($this->DATA[$name];
+    }
 }
